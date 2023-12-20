a90a1d82097637e9767724c0d065250896581a5b
sdivi <sailokesh.d@gmail.com>
2023-12-20 17:49:25 -0500

    converted compose to xml

---
+++ app/src/main/java/eu/tutorials/diceroller/MainActivity.kt
@@ -0,0 +1,12 @@
+package eu.tutorials.diceroller
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import eu.tutorials.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
